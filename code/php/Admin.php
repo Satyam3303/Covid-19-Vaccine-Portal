a2c5b1e7a06abc42d345c0f6f1018d2d9bbb289a
Satyam3303 <shivamsatyam209@gmail.com>
2022-04-18 01:59:31 +0530

    formatted document

--- code/php/Admin.php
+++ code/php/Admin.php
@@ -1,82 +1,84 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style><?php include '../css/Register.css' ?></style>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    <?php include '../css/Register.css' ?>
+  </style>
 </head>
+
 <body>
 
-<?php
+  <?php
 
-    $code="";
-    $code=$_POST['codeno'];
-    $ans="";
-    $ans=$_POST['passans'];
-	$servername = "localhost:3308";
-	$username = "root";
-	$password = "";
-	$dbname = "corona";
-	
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	
-	// Check connection
-	if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+  $code = "";
+  $code = $_POST['codeno'];
+  $ans = "";
+  $ans = $_POST['passans'];
+  $servername = "localhost:3306";
+  $username = "root";
+  $password = "";
+  $dbname = "corona";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
   }
-  $sql="SELECT * FROM doctor WHERE ID_Code='$code' AND Passcode='$ans'";
-  $result=$conn->query($sql);
-  if ($result->num_rows > 0) 
-  {
-      ?>
-<div class="form-container">
-<form action="../php/timeslots.php" method="post">
+  $sql = "SELECT * FROM doctor WHERE ID_Code='$code' AND Passcode='$ans'";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+  ?>
+    <div class="overlay"></div>
+    <div class="form-container">
+      <form action="../php/timeslots.php" method="post">
         <div class="title">
-        <h3>Choose Date and Time</h3>
-  </div>
-    <br>
-    <div class="user-details">
+          <h3>Choose Date and Time</h3>
+        </div>
+        <br>
+        <div class="user-details">
 
-    Date of Vaccination:	
+          Date of Vaccination:
 
-					<input type="date" name="pre" required="required"><br><br>
-					<label for="timeslot">Choose a Time Slot:</label>
+          <input type="date" name="pre" required="required"><br><br>
+          <label for="timeslot">Choose a Time Slot:</label>
 
-        <select name="ts" id="ts" required="required">
-          
-	  <option value="" selected disabled="disabled">--Choose A Time Slot--</option>
-    <option value="9am-12pm">9am - 12pm</option>
-    <option value="1pm-3pm">1pm - 3pm</option>
-    <option value="4pm-6pm">4pm - 6pm</option>
-    
-  </select><br>
+          <select name="ts" id="ts" required="required">
 
+            <option value="" selected disabled="disabled">--Choose A Time Slot--</option>
+            <option value="9am-12pm">9am - 12pm</option>
+            <option value="1pm-3pm">1pm - 3pm</option>
+            <option value="4pm-6pm">4pm - 6pm</option>
 
-        <input type="submit" name="sub1" class="button2"><br>
-  </div>
-</form>
-  </div>
+          </select><br>
 
 
-    <?php
-  }
+          <input type="submit" name="sub1" class="button2"><br>
+        </div>
+      </form>
+    </div>
 
 
-  else{
+  <?php
+  } else {
 
-    ?>
+  ?>
     <script>
       alert("Incorrect ID Code or Password");
-      location="../html/Admin.html";
+      location = "../html/Admin.html";
     </script>
 
-<?php
+  <?php
   }
-    ?>
+  ?>
 
-    
+
 </body>
+
 </html>
--- code/php/timeslots.php
+++ code/php/timeslots.php
@@ -3,67 +3,71 @@
 
 <head>
 
-<style><?php include '../css/Register.css' ?></style>
+    <style>
+        <?php include '../css/Register.css' ?>
+    </style>
 </head>
 <?php
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-$link = mysqli_connect("localhost:3308", "root", "", "corona");
- ?>
-<div class="container">
-<?php
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-
-
-$times ="";
-$times=$_POST['ts'];
-$dates="";
-$dates=$_POST['pre'];
-
-// Attempt select query execution
-$sql = "SELECT * FROM registration WHERE Time_Slot='$times' AND Preferred_Date='$dates'";
-
-
-if($result = mysqli_query($link, $sql)){
-
-
-    if(mysqli_num_rows($result) > 0){
-       
-while($row = mysqli_fetch_array($result)){
-            
-                echo "<b>First Name : </b>" . $row['First_name'] . "<br>";
-                echo "<b>Last Name : </b>" . $row['Last_name'] . "<br>";
-                echo "<b>Date Of Vaccination : </b>" . $row['Preferred_Date'] . "<br>";
-                echo "<b>Date Of Birth : </b>" . $row['Date_of_Birth'] . "<br>";
-                echo "<b>Time Slot : </b>" . $row['Time_Slot'] . "<br>";
-                echo "<b>Phone Number : </b>" . $row['Phone_No'] . "<br>";
-                echo "<b>State : </b>" . $row['Indian_State'] . "<br>";
-                echo "<b>Travelled Through COVID States : </b>" . $row['Covid_Affected'] . "<br>";
-                echo "<b>E-mail : </b>" . $row['Email'] . "<br>";
-                echo "<b>Gender : </b>" . $row['Gender'] . "<br>";
-
-                echo"<hr>";
-            
-        }
-       
-        // Free result set
-        mysqli_free_result($result);
-
-        
-    } else{
-        echo "No records matching your query were found.";
-    }
-} else{
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
- 
-// Close connection
-mysqli_close($link);
+$link = mysqli_connect("localhost:3306", "root", "", "corona");
 ?>
+<div class="overlay"></div>
+<div class="container10">
+    <fieldset>
+        <legend>Todays Booking details</legend>
+        <?php
+        // Check connection
+        if ($link === false) {
+            die("ERROR: Could not connect. " . mysqli_connect_error());
+        }
+
+
+        $times = "";
+        $times = $_POST['ts'];
+        $dates = "";
+        $dates = $_POST['pre'];
+
+        // Attempt select query execution
+        $sql = "SELECT * FROM registration WHERE Time_Slot='$times' AND Preferred_Date='$dates'";
+
+
+        if ($result = mysqli_query($link, $sql)) {
+
+
+            if (mysqli_num_rows($result) > 0) {
+
+                while ($row = mysqli_fetch_array($result)) {
+
+                    echo "<b>First Name : </b>" . $row['First_name'] . "<br>";
+                    echo "<b>Last Name : </b>" . $row['Last_name'] . "<br>";
+                    echo "<b>Date Of Vaccination : </b>" . $row['Preferred_Date'] . "<br>";
+                    echo "<b>Date Of Birth : </b>" . $row['Date_of_Birth'] . "<br>";
+                    echo "<b>Time Slot : </b>" . $row['Time_Slot'] . "<br>";
+                    echo "<b>Phone Number : </b>" . $row['Phone_No'] . "<br>";
+                    echo "<b>State : </b>" . $row['Indian_State'] . "<br>";
+                    echo "<b>Travelled Through COVID States : </b>" . $row['Covid_Affected'] . "<br>";
+                    echo "<b>E-mail : </b>" . $row['Email'] . "<br>";
+                    echo "<b>Gender : </b>" . $row['Gender'] . "<br>";
+
+                    echo "<hr>";
+                }
+
+                // Free result set
+                mysqli_free_result($result);
+            } else {
+                echo "No records matching your query were found.";
+            }
+        } else {
+            echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+        }
+
+        // Close connection
+        mysqli_close($link);
+        ?>
+    </fieldset>
 </div>
-    
+
 </body>
+
 </html>
